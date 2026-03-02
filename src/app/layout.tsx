517713eef5dd2d190b10ee0641e84f7a92bb2f23
Wisnu Strw <88566483+wsnustrw@users.noreply.github.com>
2026-03-02 15:38:00 +0700

    adding ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
+        <meta name="google-adsense-account" content="ca-pub-1051375345763923"></meta>
       </head>
       <body className="font-sans antialiased">
         <Analytics/>
